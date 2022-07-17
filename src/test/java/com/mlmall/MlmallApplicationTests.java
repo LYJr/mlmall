a1e864c2ea44938f5f9ce1bd7a97159199cc2a50
Yea ji Lee <yeajilee@Yeaui-MacBook-Pro.local>
2022-07-18 01:48:13 +0900

    first commit

---
+++ src/test/java/com/mlmall/MlmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
